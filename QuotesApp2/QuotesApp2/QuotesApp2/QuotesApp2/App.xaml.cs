4148300bfca0cd9c2cb30760e0adc6431ea1424d
Samuel Schumacher <samuel.schumacher@daveramsey.com>
2021-11-12 10:41:01 -0600

    Initial files

---
+++ QuotesApp2/QuotesApp2/QuotesApp2/QuotesApp2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace QuotesApp2
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
